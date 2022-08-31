205f1a03e02babec0fe7b3ee524aaa6ce3442b7b
Demouse1 <112354287+Demouse1@users.noreply.github.com>
2022-08-31 15:48:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['693593103']
+global.premium = ['693593103']
+global.ownernomer = '693593103'
+global.ownername = '👑 Demouse'
+global.botname = 'Ryomen-Sukuna'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
